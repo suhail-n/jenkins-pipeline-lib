a06906f01910641c1474778da21c64c052a47308
suhail-n <suhail.nooristani@gmail.com>
2023-08-17 00:05:19 -0400

    added buildMavenPipeline

---
+++ vars/buildMavenPipeline.groovy
@@ -0,0 +1,52 @@
+def call(config = [:]) {
+    pipeline{
+        agent {
+            label config.agent
+        }
+        tools {
+            maven 'maven-3.8.5'
+            git 'Default'
+        }
+        stages{
+            stage("Checkout the project"){
+                steps{
+                    echo "========Checkout Project========"
+                    git branch : config.gitBranch, url: config.gitUrl
+                }
+
+            }
+            stage('Build') {
+                steps {
+                    mavenCleanPackage()
+                }
+            }
+             stage('Sonar Quality Check') {
+
+                 steps {
+                     mavenSonarScan config
+                 }
+             }
+             stage("Quality Gate") {
+                 steps {
+                     sonarWaitForQualityGate
+                 }
+             }
+             stage("Building the image"){
+                 steps{
+                     buildDockerImage config
+                 }
+             }
+        }
+        post{
+            always{
+                echo "========always========"
+            }
+            success{
+                echo "========pipeline executed successfully ========"
+            }
+            failure{
+                echo "========pipeline execution failed========"
+            }
+        }
+    }
+}
